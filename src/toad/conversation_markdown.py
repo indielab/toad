ff0b21002445d8ac6bf0c1b7d2a12934f6a55500
Will McGugan <willmcgugan@gmail.com>
2025-07-18 19:37:36 +0100

    cursor

---
+++ src/toad/conversation_markdown.py
@@ -0,0 +1,18 @@
+from textual.widgets import Markdown
+from textual.widgets._markdown import MarkdownBlock
+
+
+class ConversationCodeFence(Markdown.BLOCKS["code_fence"]):
+    pass
+
+
+CUSTOM_BLOCKS = {"code_fence": ConversationCodeFence}
+
+
+class ConversationMarkdown(Markdown):
+    """Markdown widget with custom blocks."""
+
+    def get_block_class(self, block_name: str) -> type[MarkdownBlock]:
+        if (custom_block := CUSTOM_BLOCKS.get("block_name")) is not None:
+            return custom_block
+        return super().get_block_class(block_name)
